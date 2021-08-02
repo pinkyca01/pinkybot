878b296ff541b956c1cfc92d753cfd23e29de049
pinkyca01 <87077680+pinkyca01@users.noreply.github.com>
2021-08-02 22:01:35 +0700

    Add files via upload

---
+++ pinky/caa.js
@@ -0,0 +1,144 @@
+exports.allmenu = (prefix) => {
+	return`╭──「 *PINKY BOT* 」
+┴𖧹
+❑ Base: FxSx & FdlX
+❑ Recode: Pinky
+❑ Prefix: ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──「 *INFO BOT* 」
+┴𖧹
+❑ ${prefix}info
+❑ ${prefix}blocklist
+❑ ${prefix}owner
+> status
+┬𖧹
+╰────────𖧹
+
+╭──「 *GRUP* 」
+┴𖧹
+❑ ${prefix}add
+❑ ${prefix}kick
+❑ ${prefix}listadmin
+❑ ${prefix}tagall
+❑ ${prefix}totag
+┬𖧹
+╰────────𖧹
+
+╭──「 *FUNNY* 」
+┴𖧹
+❑ ${prefix}tts
+❑ ${prefix}sticker
+❑ ${prefix}trigger
+❑ ${prefix}toimg
+❑ ${prefix}hilih
+❑ ${prefix}holoh
+❑ ${prefix}tomp3
+❑ ${prefix}wait
+❑ ${prefix}ocr
+┬𖧹
+╰────────𖧹
+
+╭──「 *OWNER* 」
+┴𖧹
+❑ ${prefix}public
+❑ ${prefix}self
+❑ ${prefix}setprefix
+❑ ${prefix}setfname
+❑ ${prefix}clearchat
+❑ ${prefix}bc
+❑ ${prefix}clone
+┬𖧹
+╰────────𖧹
+`
+}
+
+exports.aboutmenu = (prefix) => {
+	return`╭──「 *PINKY BOT* 」
+┴𖧹
+❑ Base: FxSx & FdlX
+❑ Recode: Pinky
+❑ Prefix: ❑ ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──「 *INFO BOT* 」
+┴𖧹
+❑ ${prefix}info
+❑ ${prefix}blocklist
+❑ ${prefix}owner
+> status
+┬𖧹
+╰────────𖧹
+`
+}
+
+exports.grupmenu = (prefix) => {
+	return`╭──「 *PINKY BOT* 」
+┴𖧹
+❑ Base: FxSx & FdlX
+❑ Recode: Pinky
+❑ Prefix: ❑ ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──「 *GRUP* 」
+┴𖧹
+❑ ${prefix}add
+❑ ${prefix}kick
+❑ ${prefix}listadmin
+❑ ${prefix}tagall
+❑ ${prefix}totag
+┬𖧹
+╰────────𖧹
+`
+}
+
+exports.funmenu = (prefix) => {
+	return`╭──「 *PINKY BOT* 」
+┴𖧹
+❑ Base: FxSx & FdlX
+❑ Recode: Pinky
+❑ Prefix: ❑ ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──「 *FUNNY* 」
+┴𖧹
+❑ ${prefix}tts
+❑ ${prefix}sticker
+❑ ${prefix}trigger
+❑ ${prefix}toimg
+❑ ${prefix}hilih
+❑ ${prefix}holoh
+❑ ${prefix}tomp3
+❑ ${prefix}wait
+❑ ${prefix}ocr
+┬𖧹
+╰────────𖧹
+`
+}
+
+exports.ownermenu = (prefix) => {
+	return`╭──「 *PINKY BOT* 」
+┴𖧹
+❑ Base: FxSx & FdlX
+❑ Recode: Pinky
+❑ Prefix: ❑ ${prefix}
+┬𖧹
+╰────────𖧹
+
+╭──「 *OWNER* 」
+┴𖧹
+❑ ${prefix}public
+❑ ${prefix}self
+❑ ${prefix}setprefix
+❑ ${prefix}setfname
+❑ ${prefix}clearchat
+❑ ${prefix}bc
+❑ ${prefix}clone
+┬𖧹
+╰────────𖧹
+`
+}
